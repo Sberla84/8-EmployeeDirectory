ece3f9e78b594ab8ded2c89a9ae1a1260b0cc290
Niccolò Vannetti <n.vannetti@gmail.com>
2019-12-19 19:24:55 +0100

    created array of employees

--- js/main.js
+++ js/main.js
@@ -1,33 +1,60 @@
 
-const employeesUrl = 'https://randomuser.me/api/?results=12&inc=name,location,email,phone,picture&nat=us'
-
-// const employeesList2 = $.ajax({
-//     url: 'https://randomuser.me/api/?results=12&inc=name,location,email,phone,picture&nat=us',
-//     dataType: 'json',
-//     success: function(data) {
-//       console.log(data);
-//     }
-//   });
-
-    
-// const employeesList = $.ajax({
-//     url: 'http://api.open-notify.org/astros.json',
-//     dataType: 'json',
-//     success: function(data) {
-//       console.log(data);
-//     }
-//   });
-
-
+const employeesUrl = 'https://randomuser.me/api/?results=12&inc=name,location,email,phone,picture,dob&nat=us';
+const employeeList = document.getElementById('employee-wrap');
+var employeeArray = [];
 
 async function createEmployeesList(url) {
     const employeesList = await fetch(url);
-    const employeesJSON = await employeesList.json();
-    // const employeesParsed = await JSON.parse(employeesJSON);
-    alert(employeesJSON);
-
+    const employeesJSON = await employeesList.json(); 
+    
+        employeesTry = employeesJSON.results.map( async (employee) => {
+        const name = employee.name;
+        const location = employee.location;
+        const email = employee.email;
+        const phone = employee.phone;
+        const picture = employee.picture;
+        const dob = employee.dob;
+        return {name, location,email,phone,picture,dob};
+    });
+return Promise.all(employeesTry);
 }
 
 
-createEmployeesList(employeesUrl);
+
+
+function createEmployeesArray(data) {
+    data.forEach(employee => {
+        let person = {};
+        person.name = employee.name.first + ' ' + employee.name.last;
+        person.image = employee.picture.thumbnail;
+        person.email = employee.email;
+        person.city = employee.location.city;
+        person.phone = employee.phone;
+        person.address = employee.location.street.number + ' ' + employee.location.street.name;
+        person.postcode = employee.location.postcode;
+        // console.log(person);
+        employeeArray.push(person);
+    });
+}
+
+// function generateHTML(data) {
+//     data.map( employee => {
+//         const section = document.createElement('section');
+//     employeeList.appendChild(section);
+//     section.innerHTML = `
+//       <img src=${employee.picture.thumbnail}>
+//       <h2>${employee.name.first}</h2>
+//       <p>${employee.email}</p>
+//       <p>${employee.location.city}</p>
+//     `;
+//     })
+//  }
+
+
+createEmployeesList(employeesUrl)
+    // .then( data => console.log(data))
+    // .then(generateHTML)
+    .then(createEmployeesArray)
+    .catch( err => console.log(err))
+
 
