0d02c0195ccc4b817448d022e87300d215acdfaa
Niccolò Vannetti <n.vannetti@gmail.com>
2019-12-21 18:08:41 +0100

    added filter function

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 const employeesUrl = 'https://randomuser.me/api/?results=12&inc=name,location,email,phone,picture,dob&nat=us';
 const employeeList = document.getElementById('employee-wrap');
 var employeeArray = [];
-
+var $search = $('.search-box');
+var cache = [];
 
 
 // fetch data from URL
@@ -30,8 +31,8 @@ return Promise.all(employeesTry);
 function createEmployeesArray(data) {
     data.forEach(employee => {
         let person = {};
-        person.name = employee.name.first;
-        person.surname = employee.name.last;
+        person.name = employee.name.first + ' ' + employee.name.last;
+        // person.surname = employee.name.last;
         person.image = employee.picture.thumbnail;
         person.email = employee.email;
         person.city = employee.location.city;
@@ -53,7 +54,7 @@ function generateHTML(){
             employeeList.appendChild(div);
             div.innerHTML = `
             <img src=${employeeArray[x].image}>
-            <h2>${employeeArray[x].name + ' ' + employeeArray[x].surname}</h2>
+            <h2>${employeeArray[x].name }</h2>     
             <p>${employeeArray[x].email}</p>
             <p>${employeeArray[x].city}</p>
             `;
@@ -61,13 +62,42 @@ function generateHTML(){
 }
 
 
-
-
-
 createEmployeesList(employeesUrl)
-    // .then( data => console.log(data))
     .then(createEmployeesArray)
     .then(generateHTML)
     .catch( err => console.log(err))
 
 
+
+// filter functions
+
+// function filter() {
+//     var query = this.value.trim().toLowerCase();
+//     employeeArray.forEach(function(img) {
+//       var index = 0;
+//       if (query) {
+//        index = img.text.indexOf(query);
+//       }
+//         img.element.parentElement.style.display = index === -1 ? 'none' : '';
+//     });
+//   }
+
+function filter(){
+    var query = this.value.trim().toLowerCase();
+    for ( let j = 0; j < employeeArray.length; j++){
+        let name = employeeArray[j].name.toLowerCase();
+        // let surname = employeeArray[j].surname.toLowerCase(); 
+        if ( name.indexOf(query) > -1 ){
+            $('#employee-wrap').children().eq(j).show();
+        } else {
+            $('#employee-wrap').children().eq(j).hide();
+        }
+    }
+};
+
+
+  if ('oninput' in $search[0]) {
+    $search.on('input', filter);
+  } else {
+    $search.on('keyup', filter);
+  }
